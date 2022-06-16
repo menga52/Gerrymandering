048c5c3db248ad20cfcf77846c7c9b6f28b8a45f
menga52 <menga@rose-hulman.edu>
2022-06-16 18:34:05 -0400

    buggy improvement to neighborhood and simulated annealing algorithms

--- src/utilities/gerrymander.py
+++ src/utilities/gerrymander.py
@@ -5,6 +5,25 @@ from numpy.random import normal
 from classes.state import cloneMatrix
 from utilities.utilities import *
 
+
+def compute_fitness(favored, state: State, compactness_w=0, elongatedness_w=0, indentedness_w=0, puncturedness_w=1, separatedness_w=1, voting_outcome_w=5, compactness_pow=2.0):
+	"""
+	compactness
+	elongatedness
+	indentedness
+	puncturedness
+	separatedness
+	voting_outcome
+	TODO: include nonlinear voting outcome fitness (e.g. is 81% better than 80% by the same amount that 51% is better than 50%?)
+	"""
+	votes = computeVotesEqualWeight(state)
+	if votes[favored]+votes[1-favored]==0:
+		print(state.district_matrix)
+	proportion = votes[favored] / (votes[favored] + votes[1 - favored])
+	denom = compactness_w + elongatedness_w + indentedness_w + puncturedness_w + separatedness_w + voting_outcome_w
+	return compactness_w*compactness(state, compactness_pow) + elongatedness_w*elongatedness(state) + indentedness_w*indentedness(state) + puncturedness_w*puncturedness(state) + separatedness_w*separatedness(state) + voting_outcome_w*proportion / denom
+
+
 def fitness_proportional_sampling(population, num_samples, fitness_function=compute_fitness, favored=1):
 	"""
 	choose the parents for the next generation based on a supplied fitness function
@@ -129,30 +148,6 @@ def geneticGerrymander(state: State, num_districts, pop_size, combination_propor
 			index = i
 	return population[index]
 	
-def neighborhoodGerrymander(state: State, num_districts, pop_size, distance, iterations, favored=1):
-	"""
-	inputs:
-	state - the state object to be gerrymandered
-	favored - the party we are trying to artificially support
-	pop_size - the number of maps tracked in each iteration
-	num_districts - the number of districts into which the state will be divided
-	iterations - the number of times to loop
-	distance - the maximum number of random switches to be carried out in switching phase
-	
-	Note that the distance between two maps is half the number of precincts that differ between the two maps.
-		to compute it, we would iterate through each map and compute, for each map, the number of precincts with
-		differing district IDs, add them up, and divide by two. Because districts need not be constant size, 
-		distances between maps need not be whole numbers.
-		
-		We define distance in this way so that we can construct a map that has distance N from a given district
-		by swapping N precincts in each district
-	"
-	
-	strategy:
-	find initial maps
-	repeat
-	"""
-	return NotImplementedError
 	
 def reproduce(state1: State, state2: State, combination_proportion):
 	"""
@@ -211,22 +206,165 @@ def reproduce(state1: State, state2: State, combination_proportion):
 	child = state1.clone()
 	child.instantiate(district_matrix)
 	return child
-
-
-def compute_fitness(favored, state: State, compactness_w=1, elongatedness_w=1, indentedness_w=1, puncturedness_w=1, separatedness_w=1, voting_outcome_w=5, compactness_pow=2.0):
+	
+	
+def neighborhoodGerrymander(state: State, num_districts, pop_size, distance, search_area, iterations, favored=1, fitness_function=compute_fitness):
 	"""
-	compactness
-	elongatedness
-	indentedness
-	puncturedness
-	separatedness
-	voting_outcome
-	TODO: include nonlinear voting outcome fitness (e.g. is 81% better than 80% by the same amount that 51% is better than 50%?)
+	inputs:
+	state - the state object to be gerrymandered
+	favored - the party we are trying to artificially support
+	pop_size - the number of maps tracked in each iteration
+	num_districts - the number of districts into which the state will be divided
+	iterations - the number of times to loop
+	distance - the maximum number of random switches to be carried out in switching phase
+	
+	Note that the distance between two maps is half the number of precincts that differ between the two maps.
+		to compute it, we would iterate through each map and compute, for each map, the number of precincts with
+		differing district IDs, add them up, and divide by two. Because districts need not be constant size, 
+		distances between maps need not be whole numbers.
+		
+		We define distance in this way so that we can construct a map that has distance N from a given district
+		by swapping N precincts in each district
+	"
+	
+	strategy:
+	find initial maps
+	find best option in distance-neighborhood of size search_area (not exhaustive)
+	update, repeat
+	
+	note that this algorithm carries out pop_size identical searches at once
 	"""
-	votes = computeVotesEqualWeight(state)
-	proportion = votes[favored] / (votes[favored] + votes[1 - favored])
-	denom = compactness_w + elongatedness_w + indentedness_w + puncturedness_w + separatedness_w + voting_outcome_w
-	return compactness_w*compactness(state, compactness_pow) + elongatedness_w*elongatedness(state) + indentedness_w*indentedness(state) + puncturedness_w*puncturedness(state) + separatedness_w*separatedness(state) + voting_outcome_w*proportion / denom
+	if(num_districts < 2):
+		raise ValueError("not implemented for fewer than two districts. Trivial anyway")
+	
+	population = [0]*pop_size
+	init_map = resetMatrix(cloneMatrix(state.voting_outcome))
+	for i in range(pop_size):
+		population[i] = fmap(init_map, num_districts, 0)
+	# we now have a population of random maps
+	
+	for i in range(pop_size):
+		temp_pop = [0]*search_area
+		for j in range(search_area):
+			temp_map = population[i]
+			for k in range(distance):
+				# pick a random precinct and switch it to a random neighboring district
+				switch_precinct(temp_map)
+			temp_pop[j] = temp_map # after all of the switches, 
+		max_fitness = float('-inf')
+		max_index = -1
+		# choose the best in the search area to take the place of the original in the population
+		for j in range(search_area):
+			clone = state.clone()
+			clone.instantiate(temp_pop[j])
+			current_fitness = fitness_function(favored, clone)
+			if current_fitness > max_fitness:
+				max_fitness = current_fitness
+				max_index = j
+		population[i] = temp_pop[max_index]
+	max_fitness = float('-inf')
+	max_index = -1
+	# return the best map in the population
+	for i in range(pop_size):
+		clone = state.clone()
+		clone.instantiate(population[i])
+		current_fitness = fitness_function(favored, clone)
+		if current_fitness > max_fitness:
+			max_fitness = current_fitness
+			max_index = i
+	
+	# clone the original state and return the clone instantiated with the best available map
+	clone = state.clone()
+	clone.instantiate(population[max_index])
+	return clone
+	
+
+def switch_precinct(district_matrix):
+	# choose a random precinct in the district (not necessarily uniformly, but constant time)
+	x = int(random()*len(district_matrix))
+	y = int(random()*len(district_matrix[x]))
+	if isCutVertex((x,y), district_matrix):
+		# print(x)
+		# print(y)
+		return switch_precinct(district_matrix)
+	original_district = district_matrix[x][y] # store the previous district ID of that precinct
+	neighboring_precincts = [(x-1,y),(x,y-1),(x+1,y),(x,y+1)]
+	neighboring_districts = []
+	solo_precinct = True # we don't want to eliminate a whole district, so we make sure it isn't alone
+	for n in neighboring_precincts:
+		# we iterate through the neighbors to find potential districts to which to add this precinct
+		if is_matched(n, district_matrix):
+			district_to_add = district_matrix[n[0]][n[1]]
+			if district_to_add == original_district:
+				solo_precinct = False # now we know it is okay to switch this precinct around
+			elif district_to_add not in neighboring_districts:
+				neighboring_districts.append(district_to_add)
+	if solo_precinct:
+		# if this precinct is the whole district, choose another precinct
+		return switch_precinct(district_matrix)
+	if neighboring_districts == []:
+		# if there are no candidates (we didn't choose a bordering district), try again
+		# note that this recurses infinitely if the entire map is a single district
+		return switch_precinct(district_matrix)
+	rand_index = int(random()*len(neighboring_districts))
+	district_matrix[x][y] = neighboring_districts[rand_index]
+	# perform the switch and return the precinct tuple and original district
+	return [x, y, original_district]
+	
+	
+def example_cool(temp):
+	return 0.995*temp
+	
+	
+def simulatedAnnealingGerrymander(state: State, num_districts, initial_temp, iterations, cooling_function=example_cool, favored=1, fitness_function=compute_fitness):
+	"""
+	gerrymanders a state using a simulated annealing approach
+	
+	inputs:
+	state - the State object to be gerrymandered
+	num_districts - the number of districts which will exist in the map
+	initial_temp - the initial_temperature
+	iterations - the number of jumps to be made 
+	cooling_function - a function which computes the next temperature given a current temp
+		next_temp = cooling_function(current_temp)
+	favored - the party to be favored (default 1)
+	
+	output:
+	clone - the gerrymandered state object
+	
+	algorithm outline: 
+	1. choose a random initial map
+	2. repeatedly:
+			a. switch a precinct to a neighboring district
+			b. if its better, keep the switch
+			c. sometimes, but less frequently as time goes on, make the switch anyway
+	3. return. eventually. Very likely to find a local minimum (as it simplifies to a neighborhood search eventually)
+	
+	TODO: allow for greater search radius (multiple switches before testing)
+				difficult part of this will be returning the map to its original without cloning (slow)
+	"""
+	current_map = fmap(resetMatrix(cloneMatrix(state.voting_outcome)), num_districts, 0) # choose original map
+	clone = state.clone()
+	clone.instantiate(current_map)
+	current_fitness = fitness_function(favored, clone) # find its fitness
+	current_temp = initial_temp
+	for i in range(iterations):
+		x, y, original_district = switch_precinct(current_map) # make a switch
+		clone = state.clone()
+		clone.instantiate(current_map)
+		candidate_fitness = fitness_function(favored, clone) # compute its new fitness
+		if candidate_fitness < current_fitness or random() < current_temp: 
+			# current_map already updated
+			current_fitness = candidate_fitness
+		else:
+			# return the precinct to its original district
+			current_map[x][y] = original_district
+		current_temp = cooling_function(current_temp) #update the temperature
+	
+	# return a cloned state object instantiated with the resultant district map
+	clone = state.clone()
+	clone.instantiate(current_map)
+	return clone
 		
 	
 def fmap(matr, num_districts, districts_assigned):
@@ -266,6 +404,22 @@ def fmap(matr, num_districts, districts_assigned):
 		matr[x][y] = chosen_option
 		del candidates[candidate_index]
 	return matr
+
+
+def isCutVertex(ordered_pair, district_matrix):
+	x=ordered_pair[0]; y=ordered_pair[1]
+	original_district = district_matrix[x][y]
+	def fun(n, matr):
+		if is_matched(n, matr):
+			if matr[n[0]][n[1]] == original_district:
+				return True
+		return False
+	district_matrix[x][y] = -1
+	print("before")
+	groups = contiguousGroups(fun, district_matrix)
+	print("after")
+	district_matrix[x][y] = original_district
+	return len(groups) > 1
 
 
 def update_district_matrix(district_matrix, district_list, district_ID):
@@ -334,7 +488,7 @@ def getStart(fun, matr):
 	
 def contiguousGroups(fun, matr):
 	"""
-	helper - repeatedly 
+	helper - repeatedly call bfs, find all continuous groups of precincts which satisfy fun
 	"""
 	groups = []
 	start = getStart(fun, matr)
